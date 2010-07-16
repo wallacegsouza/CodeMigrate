df8499cd4832087004e30ac06292d6c1d80f38e2
Bruno Arueira <bgnarueira@gmail.com>
2010-07-16 11:47:13 -0300

    Remoção de lixo de conflito de versões na classe manageCode

--- src/br/manageCode.java
+++ src/br/manageCode.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 public class manageCode {
 
-<<<<<<< HEAD
     HashMap<String,Entidade> mapEntidade = new HashMap<String, Entidade>();
     List<String> tipos = new ArrayList<String>();
 
